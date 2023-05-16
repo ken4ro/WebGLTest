acb76577206dba808e3a4fd99509958128beb638
ken4ro <zwei_bird@hotmail.com>
2023-05-16 17:50:02 +0900

    Sora関連コード不具合修正。

--- Projects/App/src/components/SoraCanvas.tsx
+++ Projects/App/src/components/SoraCanvas.tsx
@@ -12,13 +12,12 @@ export const SoraCanvas = () => {
     const [connect, setConnect] = useState<boolean>(false);
     const [sendrecv, setSendrecv] = useState<ConnectionPublisher>();
     const [node, setNode] = useState<AudioWorkletNode>();
-    const initRef = useRef<boolean>(false);
     const remoteVideoRef = useRef<HTMLVideoElement>(null);
     const remoteVideoIdRef = useRef<HTMLParagraphElement>(null);
     const volumeTextRef = useRef<HTMLSpanElement>(null);
 
-    // WebRTC接続ハンドラ
-    const Connect = async () => {
+    // Startボタン処理
+    const ClickStartSendRecv = async () => {
         // mediastream接続
         window.console.log("on webrtc_connect event");
         const mediaStream = await window.navigator.mediaDevices.getUserMedia({ audio: true, video: true });
@@ -28,8 +27,8 @@ export const SoraCanvas = () => {
         setConnect(true);
     };
 
-    // WebRTC切断ハンドラ
-    const Disconnect = async () => {
+    // Stopボタン処理
+    const ClickStopSendRecv = async () => {
         // mediastream切断
         window.console.log("on webrtc_dicconnect event");
         if (sendrecv) {
@@ -41,89 +40,96 @@ export const SoraCanvas = () => {
         setConnect(false);
     };
 
-    // Startボタン処理
-    const ClickStartSendRecv = async () => {
-        // mediastream接続
-        Connect();
-    };
-
-    // Stopボタン処理
-    const ClickStopSendRecv = async () => {
-        // mediastream切断
-        Disconnect();
-    };
-
     useEffect(() => {
-        // 開発時はStrictModeにより2度呼ばれるので回避
-        if (!initRef.current) {
-            initRef.current = true;
+        // Soraインスタンス生成
+        const sendrecv = SoraProvider();
+        setSendrecv(sendrecv);
 
-            // Soraインスタンス生成
-            const sendrecv = SoraProvider();
-            setSendrecv(sendrecv);
+        // WebRTC接続ハンドラ
+        const Connect = async () => {
+            // mediastream接続
+            window.console.log("on webrtc_connect event");
+            const mediaStream = await window.navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+            if (sendrecv) {
+                await sendrecv.connect(mediaStream);
+            }
+            setConnect(true);
+        };
 
-            // 接続したチャネルIDにMediaStreamが追加された
-            sendrecv.on("track", async (event) => {
-                const stream = event.streams[0];
-                if (!stream) {
-                    window.console.log(`on track error!`);
-                    return;
-                }
-                if (event.track.kind === "video") {
-                    // 接続相手のカメラを描画
-                    window.console.log(`Add mediastream video track: ${stream.id}`);
-                    if (remoteVideoRef.current) {
-                        remoteVideoRef.current.style.border = "1px solid red";
-                        remoteVideoRef.current.autoplay = true;
-                        remoteVideoRef.current.playsInline = true;
-                        remoteVideoRef.current.controls = true;
-                        remoteVideoRef.current.width = 160;
-                        remoteVideoRef.current.height = 120;
-                        remoteVideoRef.current.srcObject = stream;
-                    }
-                    if (remoteVideoIdRef.current) {
-                        remoteVideoIdRef.current.innerText = stream.id;
-                    }
-                } else if (event.track.kind === "audio") {
-                    window.console.log(`Add mediastream audio track: ${stream.id}`);
-                    // 接続相手のマイク音量をUnityに送信
-                    const node = await GetVolumeNode(stream);
-                    node.port.onmessage = (ev) => {
-                        const volume = ev.data.volume;
-                        if (unityInstanceRef.current) {
-                            unityInstanceRef.current.SendMessage("GameManager", "SetVoiceVolume", volume * 10);
-                        }
-                        if (volumeTextRef.current) {
-                            volumeTextRef.current.innerText = volume;
-                        }
-                    };
-                    setNode(node);
-                }
-            });
+        // WebRTC切断ハンドラ
+        const Disconnect = async () => {
+            // mediastream切断
+            window.console.log("on webrtc_dicconnect event");
+            if (sendrecv) {
+                await sendrecv.disconnect();
+            }
+            if (remoteVideoRef.current) {
+                remoteVideoRef.current.srcObject = null;
+            }
+            setConnect(false);
+        };
 
-            // 接続したチャネルIDからMediaStreamが削除された
-            sendrecv.on("removetrack", (event) => {
-                // リモートビデオ再生停止
-                if (event.target) {
-                    // window.console.log(`Remove mediastream track: ${event.target.id}`);
-                }
+        // 接続したチャネルIDにMediaStreamが追加された
+        sendrecv.on("track", async (event) => {
+            const stream = event.streams[0];
+            if (!stream) {
+                window.console.log(`on track error!`);
+                return;
+            }
+            if (event.track.kind === "video") {
+                // 接続相手のカメラを描画
+                window.console.log(`Add mediastream video track: ${stream.id}`);
                 if (remoteVideoRef.current) {
-                    remoteVideoRef.current.srcObject = null;
+                    remoteVideoRef.current.style.border = "1px solid red";
+                    remoteVideoRef.current.autoplay = true;
+                    remoteVideoRef.current.playsInline = true;
+                    remoteVideoRef.current.controls = true;
+                    remoteVideoRef.current.width = 160;
+                    remoteVideoRef.current.height = 120;
+                    remoteVideoRef.current.srcObject = stream;
                 }
-                if (node) {
-                    node.port.onmessage = () => {
-                        if (volumeTextRef.current) {
-                            volumeTextRef.current.innerText = "";
-                        }
-                    };
-                    setNode(undefined);
+                if (remoteVideoIdRef.current) {
+                    remoteVideoIdRef.current.innerText = stream.id;
                 }
-            });
+            } else if (event.track.kind === "audio") {
+                window.console.log(`Add mediastream audio track: ${stream.id}`);
+                // 接続相手のマイク音量をUnityに送信
+                const node = await GetVolumeNode(stream);
+                node.port.onmessage = (ev) => {
+                    const volume = ev.data.volume;
+                    if (unityInstanceRef.current) {
+                        unityInstanceRef.current.SendMessage("GameManager", "SetVoiceVolume", volume * 10);
+                    }
+                    if (volumeTextRef.current) {
+                        volumeTextRef.current.innerText = volume;
+                    }
+                };
+                setNode(node);
+            }
+        });
 
-            // WebRTCイベント購読(Unityから発行)
-            window.addEventListener("webrtc_connect", Connect);
-            window.addEventListener("webrtc_disconnect", Disconnect);
-        }
+        // 接続したチャネルIDからMediaStreamが削除された
+        sendrecv.on("removetrack", (event) => {
+            // リモートビデオ再生停止
+            if (event.target) {
+                // window.console.log(`Remove mediastream track: ${event.target.id}`);
+            }
+            if (remoteVideoRef.current) {
+                remoteVideoRef.current.srcObject = null;
+            }
+            if (node) {
+                node.port.onmessage = () => {
+                    if (volumeTextRef.current) {
+                        volumeTextRef.current.innerText = "";
+                    }
+                };
+                setNode(undefined);
+            }
+        });
+
+        // WebRTCイベント購読(Unityから発行)
+        window.addEventListener("webrtc_connect", Connect);
+        window.addEventListener("webrtc_disconnect", Disconnect);
 
         // クリーンアップ
         return () => {
